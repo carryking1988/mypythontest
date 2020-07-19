2c3284984feddff3695e86c8391953068286a8de
binjia <carryking1988@gmail.com>
2020-07-18 19:56:21 -0700

    my update on code

--- myfifthtest.py
+++ myfifthtest.py
@@ -1,11 +1,5 @@
 import random
 import string
-def randomcode(n):
-    code_list = string.ascii_uppercase + string.digits
-    code = ""
-    for i in range(n):
-        code += random.choice(code_list)
-    return code 
 
-
-print(randomcode(10))
+def randomcode(size=6,chars = string.ascii_uppercase+string.digits):
+     return "".join(random.choices(chars,k=size))
