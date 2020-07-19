21fa0a5f845ca864d0117afe99eb1659fe499efe
binjia <carryking1988@gmail.com>
2020-07-18 19:26:45 -0700

    delete the randomcode function

--- myfifthtest.py
+++
@@ -1,9 +0,0 @@
-import random
-import string
-
-def randomcode(n):
-    charlist = string.ascii_uppercase + string.digits
-    return ''.join(random.choice(charlist) for i in range(n))
-
-if __name__ == "__main__":
-    print(randomcode(10))
